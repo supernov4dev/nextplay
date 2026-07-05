dc216b60d3c7e5aee2db9a7352577f23339d98c0
Romain Masseboeuf <supernov4dev@gmail.com>
2026-07-05 20:02:04 +0200

    feat: flux d'ajout (recherche IGDB, fiche manuelle, mode série)

---
+++ src/app/ajouter/page.tsx
@@ -0,0 +1,10 @@
+import { AddGameFlow } from '@/components/AddGameFlow'
+
+export default function AjouterPage() {
+  return (
+    <div className="space-y-4">
+      <h1 className="text-xl font-bold">Ajouter un jeu</h1>
+      <AddGameFlow />
+    </div>
+  )
+}
